f2aa438309dbe9f02ad9cdc2e460d68187a255f4
Nitish Kushwaha <nitish.headfirst@outlook.com>
2022-03-14 10:01:21 +0530

    render tasks in separate lists

---
+++ src/components/todo/TaskItem.jsx
@@ -0,0 +1,31 @@
+import classes from "./TaskItem.module.css";
+import Checked from "./icons/checked.png";
+import Bin from "./icons/bin.png";
+import TaskContext from "../../context/task-ctx";
+import { useContext } from "react";
+
+const TaskItem = ({ tasks }) => {
+  const ctx = useContext(TaskContext);
+
+  return (
+    <ul className={classes.list}>
+      {tasks.length === 0 ? (
+        <p>Nothing here yet.</p>
+      ) : (
+        tasks.map(({ id, title, done }) => (
+          <li key={id}>
+            <button onClick={() => ctx.check(id)}>
+              <img src={Checked} alt="check" />
+            </button>
+            <h2>{(done && <strike>{title}</strike>) || title}</h2>
+            <button onClick={() => ctx.deleteItem(id)}>
+              <img src={Bin} alt="delete" />
+            </button>
+          </li>
+        ))
+      )}
+    </ul>
+  );
+};
+
+export default TaskItem;
