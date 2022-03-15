0535402fc88204a3ce84ff5e597d86bdf350502b
Nitish Kushwaha <nitish.headfirst@outlook.com>
2022-03-15 18:44:11 +0530

    add title update functionality

--- src/components/todo/TaskItem.jsx
+++ src/components/todo/TaskItem.jsx
@@ -1,12 +1,30 @@
 import classes from "./TaskItem.module.css";
-import Checked from "./icons/checked.png";
-import Bin from "./icons/bin.png";
-import Uncheck from "./icons/uncheck.png";
+import { useState, useRef, useContext } from "react";
 import TaskContext from "../../context/task-ctx";
-import { useContext } from "react";
+import Modal from "../UI/Modal";
+import { Check, Edit, Uncheck, Bin, Cancel, Update } from "./icons/Icons";
 
 const TaskItem = ({ tasks }) => {
   const ctx = useContext(TaskContext);
+  const newTitleRef = useRef();
+  const [modal, setModal] = useState(false);
+
+  const visibilityHandler = () => {
+    setModal((modal) => !modal);
+  };
+
+  const submitHandler = (id) => {
+    const newTitle = newTitleRef.current.value;
+    if (newTitle.trim() === "") {
+      newTitleRef.current.value = "";
+      visibilityHandler();
+      return;
+    }
+
+    ctx.updateTask(id, newTitle, false);
+    newTitleRef.current.value = "";
+    visibilityHandler();
+  };
 
   return (
     <ul className={classes.list}>
@@ -15,13 +33,38 @@ const TaskItem = ({ tasks }) => {
       ) : (
         tasks.map(({ id, title, done }) => (
           <li key={id}>
-            <button onClick={() => ctx.check(id)}>
-              <img src={done ? Uncheck : Checked} alt="check" />
-            </button>
             <h2>{(done && <strike>{title}</strike>) || title}</h2>
-            <button onClick={() => ctx.deleteItem(id)}>
-              <img src={Bin} alt="delete" />
-            </button>
+            <span>
+              <button onClick={() => ctx.updateTask(id, null, true)}>
+                <img src={done ? Uncheck : Check} alt="check" />
+              </button>
+              <button onClick={visibilityHandler}>
+                <img src={Edit} alt="edit" />
+              </button>
+
+              {modal && (
+                <Modal className={classes.modal} onCancel={visibilityHandler}>
+                  <form onSubmit={() => submitHandler(id)}>
+                    <label>
+                      <h2> Enter new title</h2>
+                    </label>
+                    <input autoFocus ref={newTitleRef} type="text" />
+                    <div>
+                      <button type="reset" onClick={visibilityHandler}>
+                        <img src={Cancel} alt="cancel" />
+                      </button>
+                      <button type="submit">
+                        <img src={Update} alt="update" />
+                      </button>
+                    </div>
+                  </form>
+                </Modal>
+              )}
+
+              <button onClick={() => ctx.deleteTask(id)}>
+                <img src={Bin} alt="delete" />
+              </button>
+            </span>
           </li>
         ))
       )}
