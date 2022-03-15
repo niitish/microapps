b90de79560e8b15eb781f3481e2797854c612dec
Nitish Kushwaha <nitish.headfirst@outlook.com>
2022-03-15 18:45:13 +0530

    export icons

---
+++ src/components/todo/icons/Icons.js
@@ -0,0 +1,6 @@
+export { default as Check } from "./checked.png";
+export { default as Bin } from "./bin.png";
+export { default as Uncheck } from "./uncheck.png";
+export { default as Edit } from "./edit.png";
+export { default as Cancel } from "./cancel.png";
+export { default as Update } from "./update.png";
